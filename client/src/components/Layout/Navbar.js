5c34c9d5e6781fa9c80a3c1eaaf973cc55b2ee9f
Damir Kolobaric <dkolobaric@gmail.com>
2023-07-15 11:31:19 +0200

    Navbar

---
+++ client/src/components/Layout/Navbar.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom"
+import { NavbarData } from './NavbarData';
+import * as FaIcons from "react-icons/fa6";
+import * as MdIcons from 'react-icons/md'
+import './Navbar.css'
+import { IconContext } from 'react-icons/lib';
+
+
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+  const toggleSidebar = () => { setSidebar(!sidebar) };
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='#' className='navbar-logo'>
+            <MdIcons.MdFastfood onClick={toggleSidebar} /><span>Food App</span>
+          </Link>
+          <nav className='navbar-menu hide-navbar-menu' >
+            <ul className='navbar-menu-inline'>
+              {NavbarData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>{item.title}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
+          <FaIcons.FaBars className='menu-bars hide-menu-bars' onClick={toggleSidebar} />
+        </div>
+
+        <nav className={sidebar ? 'nav-menu-sidebar active' : 'nav-menu-sidebar'} >
+          <ul className='nav-menu-sidebar-items'>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <FaIcons.FaXmark onClick={toggleSidebar} />
+              </Link>
+            </li>
+            {NavbarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>{item.icon}<span>{item.title}</span></Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  )
+}
+
+export default Navbar
---
+++ client/src/components/Layout/NavbarData.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import * as FaIcons from "react-icons/fa6";
+
+export const NavbarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <FaIcons.FaBars />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'About',
+    path: '/',
+    icon: <FaIcons.FaBars />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Service',
+    path: '/',
+    icon: <FaIcons.FaBars />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Menu',
+    path: '/',
+    icon: <FaIcons.FaBars />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Contact',
+    path: '/',
+    icon: <FaIcons.FaBars />,
+    cName: 'nav-text'
+  }
+]
